25b61bb8a6772e246fa016526fa77ca1ae0f8714
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-10 19:03:38 +0300

    Delete fastapi_app/responses/routes directory

--- fastapi_app/responses/routes/admin.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from fastapi_app.routes.companies import path as companies
-from fastapi_app.routes.keys import path as keys
-
-router = APIRouter()
-
-router.include_router(companies.router, tags=["companies"], prefix="/companies")
-router.include_router(keys.router, tags=["api_keys"], prefix="/api_keys")
--- fastapi_app/responses/routes/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from fastapi_app.routes import api_routes, healthcheck
-
-router = APIRouter()
-router.include_router(api_routes.router, tags=["assistant"])
-router.include_router(healthcheck.router, tags=["healthcheck"], prefix="/healthcheck")
-
