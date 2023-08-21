b0e996cef6ed3d1cbee8d412beab083589a2949d
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-21 18:55:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title('Привет, мир!')
 st.write('Это мой умный корпоративный ассистент.')
+
+# Создаем форму для ввода данных пользователем
+with st.form(key='my_form'):
+    text_input = st.text_input(label='Введите текст')
+    submit_button = st.form_submit_button(label='Отправить')
+
+# Выводим данные, введенные пользователем
+if text_input:
+    st.write(f'Вы ввели: {text_input}')
