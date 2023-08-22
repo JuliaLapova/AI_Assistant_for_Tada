668baa0e157b23ddc60cfa54d369c3d01c2d7c14
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-22 12:34:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,14 @@ with st.form(key='my_form'):
     submit_button = st.form_submit_button(label='Отправить')
 
 # Выводим данные, введенные пользователем
+#if text_input:
+#    st.write(f'Вы ввели: {text_input}')
+#    answer = get_answer(document = text_input)
+#    st.write(f'Ваш ответ: {answer}')
+
+# Выводим данные, введенные пользователем
 if text_input:
     st.write(f'Вы ввели: {text_input}')
-    answer = get_answer(document = text_input)
+    answer = get_answer_with_sources(document = text_input)
     st.write(f'Ваш ответ: {answer}')
+    st.write(f'Источники: {sources}')
