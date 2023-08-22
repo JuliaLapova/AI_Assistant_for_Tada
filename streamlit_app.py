e7ec2adc1225037aa8a9cb7e559ecc14466c3c90
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-22 14:34:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,19 @@ st.write('Это мой умный корпоративный ассистент
 with st.form(key='my_form'):
     text_input = st.text_input(label='Введите текст')
     submit_button = st.form_submit_button(label='Отправить')
-
+    
 # Выводим данные, введенные пользователем
 #if text_input:
 #    st.write(f'Вы ввели: {text_input}')
 #    answer = get_answer(document = text_input)
 #    st.write(f'Ваш ответ: {answer}')
+
+user_input = text_input
+api_key = "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"
+topic = "business"
+translate_answer = False # передайте True, если вы хотите переводить ответ
+
+answer = get_answer_with_sources(user_input, api_key, topic, translate_answer)
 
 # Выводим данные, введенные пользователем
 if text_input:
