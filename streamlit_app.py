cbdfb86baad82cbd479af88ee12060fe0ea7bf27
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-22 18:35:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,34 @@ import streamlit as st
 from fastapi_app.chatbot.assistant import get_answer
 from fastapi_app.routes.api_routes import get_answer_with_sources
 
+import requests
+
+@st.cache(allow_output_mutation=True)
+def get_data_file(url):
+    localpath = "/tmp/tempfile"
+    r = requests.get(url)
+    with open(localpath, 'wb') as f:
+        f.write(r.content)
+    return localpath
+
+def main():
+    file_url = 'https://github.com/JuliaLapova/AI-Assistant/raw/main/faiss/faiss-hr/index.faiss'
+#    file_url = 'https://github.com/JuliaLapova/AI-Assistant/raw/main/faiss/faiss-hr/index.faiss'
+    localfile = get_data_file(file_url)
+
+    # Дальше вы можете использовать localfile в вашем приложении, как если бы это был локальный файл
+
+if __name__ == "__main__":
+    main()
+
+    # Выводим путь к файлу
+    st.write(f'localfile: {localfile}')
+
+    # Проверяем наличие файла и выводим результат проверки
+    st.write(f'Проверяем наличие файла: {})    
+    st.write(os.path.exists(localfile))
+
+
 st.title('Привет, мир!')
 st.write('Это мой умный корпоративный ассистент.')
 
