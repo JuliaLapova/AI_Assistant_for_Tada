3131d308ab4e7ce9bbeab2d95b2477c6b65771ec
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-24 21:58:34 +0300

    Update servies.py

--- fastapi_app/routes/user_response/servies.py
+++ fastapi_app/routes/user_response/servies.py
@@ -8,7 +8,8 @@ from sqlalchemy import select, insert, update
 from loguru import logger
 from starlette import status
 
-from fastapi_app.sql_tools import models
+#from fastapi_app.sql_tools import models
+from sql_tools import models
 from .schemas import UserResponse, UserResponseCreate, FeedbackCreate, Feedback
 from ...sql_tools.models import engine
 
