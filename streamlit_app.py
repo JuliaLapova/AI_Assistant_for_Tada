48f1623d9ba592a0a42d307414467b17b7ca8dc9
JuliaLapova <119416226+JuliaLapova@users.noreply.github.com>
2023-08-27 00:50:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,19 +46,17 @@ with col1:
     st.write('Интеллектуальный консультант для сотрудников СПб ГКУ «МФЦ»')
 
 with col2:
-#    https://drive.google.com/file/d/1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr/view?usp=drive_link
-    st.image('https://drive.google.com/uc?export=view&id=1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr')
-#    current_dir = os.getcwd()
-#    relative_path = '/images/logo.jpg'
-#    image_path = os.path.join(current_dir, relative_path)
+    st.markdown("""
+        <div style="padding-left:20px;">
+            <img src="https://drive.google.com/uc?export=view&id=1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr">
+        </div>
+        """, unsafe_allow_html=True)
 
-#st.image(image_path)
-#st.image('sftp://root@31.129.97.70/root/ai-assistant/images/logo.jpg')
-#st.image('.ai-assistant/images/logo.jpg')
-#st.image('.root/ai-assistant/images/logo.jpg')
-#st.image('.images/logo.jpg')
-#st.image('/root/ai-assistant/images/logo.jpg')
-#st.image('https://drive.google.com/file/d/1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr/view?usp=drive_link')
+
+
+#with col2:
+#    st.image('https://drive.google.com/uc?export=view&id=1g0KmrIILmwb0Vrxt-J-n0HFvQBDhk0Lr')
+
 
 # Создаем форму для ввода данных пользователем
 with st.form(key='my_form'):
